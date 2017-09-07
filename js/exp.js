5dcaa62fa16985762640a9841904a3f26b08c472
kmurray30 <kyle.murray100@gmail.com>
2017-09-07 18:29:23 -0400

    added all icons and pictures

--- js/exp.js
+++ js/exp.js
@@ -1,12 +1,17 @@
 // makeActive(document.getElementById("exp1"));
 
+makeActive(document.getElementById("exp1"));
+
 function makeActive(element) {
+	var isFirst = element.classList ? true : false;
+	var element = isFirst ? element : this;
 	var isActive = element.classList.contains("active");
 	var rPane = document.getElementById("right-pane");
 	var infoEl = document.getElementById("empty-info");
 	element.style.transition = "all 0.1s linear 0s";
-	for (exp of document.getElementsByClassName("exp")) {
-		exp.classList.remove("active");
+	expList = document.getElementsByClassName("exp");
+	for (var i = 0; i < expList.length; i++) {
+	  	expList[i].classList.remove("active");
 	}
 	// document.getElementById("right-pane").style.background = "#c0745d";
 	if (isActive) {
@@ -37,15 +42,62 @@ function makeActive(element) {
 		}
 	}
 	rPane.innerHTML = infoEl.innerHTML;
+	if (!isFirst) {
+		scrollToItem(rPane);
+	}
 }
 
+document.addEventListener('keydown', function(event) {
+	// 37: Left, 38: Up, 39: Right, 40: Down
+    if(event.keyCode == 37 || event.keyCode == 38) {
+        expList = document.getElementsByClassName("exp");
+        var activeInd = -1;
+		for (var i = 0; i < expList.length; i++) {
+		  	if (expList[i].classList.contains("active")) {
+		  		activeInd = i;
+		  	}
+		}
+		makeActive(expList[(activeInd - 1 + expList.length)%expList.length]);
+    }
+    else if(event.keyCode == 39 || event.keyCode == 40) {
+        expList = document.getElementsByClassName("exp");
+        var activeInd = -1;
+		for (var i = 0; i < expList.length; i++) {
+		  	if (expList[i].classList.contains("active")) {
+		  		activeInd = i;
+		  	}
+		}
+		makeActive(expList[(activeInd + 1)%expList.length]);
+    }
+});
 
-function onHover(element) {
-	element.classList.add("hovered");
+function makeHover(element) {
+	this.classList.add("hovered");
 }
 
 function dehover(element) {
-	for (exp of document.getElementsByClassName("exp")) {
-		exp.classList.remove("hovered");
-	}
+	this.classList.remove("hovered");
 }
+
+function scrollToItem(item) {
+    var diff=(item.offsetTop-window.scrollY)/20;
+    if(!window._lastDiff){
+        window._lastDiff = 0;
+    }
+
+    if (Math.abs(diff)>0) {
+        window.scrollTo(0, (window.scrollY+diff))
+        clearTimeout(window._TO)
+
+        if(diff !== window._lastDiff){
+            window._lastDiff = diff;
+            window._TO=setTimeout(scrollToItem, 1, item);
+        }
+    } else {
+        window.scrollTo(0, item.offsetTop)
+    }
+}
+
+$('.exp').on('mousedown', makeActive)
+$('.exp').on('mouseover', makeHover)
+$('.exp').on('mouseout', dehover)
